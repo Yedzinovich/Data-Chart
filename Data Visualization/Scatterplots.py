49022023c574e99dd38f67bfafeec821d495c96d
Inna Yedzinovich <innayedzinovich@gmail.com>
2019-07-08 18:33:20 -0400

    Scatterplot

---
+++ Data Visualization/Scatterplots.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+friends = [70, 65, 72, 63, 71, 64, 60, 64, 67]
+munites = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+plt.scatter(friends, munites)
+
+#label each point
+for label, friend_count, munite_count, in zip(labels, friends, munites):
+    plt.annotate(label,
+        xy=(friend_count, munite_count),
+        xytext=(5, -5),
+        textcoords='offset points')
+
+plt.title("Daily Minutes vs. Number of Friends")
+plt.xlabel("# of friends")
+plt.ylabel("daily minutes spent on the site")
+plt.show()
