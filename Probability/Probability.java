9097c8df966ceb5fe4164f7825c251a72a8497db
INNA YEDZINOVICH <innayedzinovich@gmail.com>
2019-08-05 15:10:23 -0400

    Update Probability.java

--- Probability/Probability.java
+++ Probability/Probability.java
@@ -1,4 +1,6 @@
 import random
+import math
+from matplotlib import pyplot as plt
 
 def random_kid():
   return random.choice(["boy", "girl"])
@@ -21,3 +23,28 @@ for _ in range(10000):
 
 print("P(both | older): ", both_girls / older_girl)      
 print("P(both | either): ", both_girls / either_girl)
+
+def unform_pdf(x):
+  return 1 if x >= 0 and x < 1 else 0
+
+def uniform_cdf(x):
+  if x < 0: 
+    return 0
+  elif x < 1:
+    return x
+  else:
+    return 1
+
+def normal_pdf(x, mu = 0, sigma = 1):
+  sqrt_two_pi = math.sqrt(2 * math.pi)
+  return (math.exp(-(x-mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
+
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs,[normal_pdf(x,sigma=1) for x in xs],'-',label='mu=0,sigma=1')
+plt.plot(xs,[normal_pdf(x,sigma=2) for x in xs],'--',label='mu=0,sigma=2')
+plt.plot(xs,[normal_pdf(x,sigma=0.5) for x in xs],':',label='mu=0,sigma=0.5')
+plt.plot(xs,[normal_pdf(x,mu=-1) for x in xs],'-.',label='mu=-1,sigma=1')
+plt.legend()
+plt.title("Various Normal pdfs")
+plt.show()
+
